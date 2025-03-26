5746e3345691d5ccab6595045c2d143d41bf99f1
AbeerAlmadadha <abeer.r.almadadha@gmail.com>
2025-03-26 17:40:00 +0300

    Update append_it.py

--- module7/ex03/append_it.py
+++ module7/ex03/append_it.py
@@ -1,1 +1,11 @@
+#!/usr/bin/env python3
+import sys
 
+i = 1
+if len(sys.argv) == 1:
+	print("none")
+else:
+	while i < len(sys.argv):
+		if not (sys.argv[i].endswith("ism")):
+			print(sys.argv[i] + "ism")
+		i += 1 
