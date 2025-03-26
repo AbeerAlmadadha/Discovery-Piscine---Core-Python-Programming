b8e859f2cfa2a9e7d89ffd5032f7764532568c2c
AbeerAlmadadha <abeer.r.almadadha@gmail.com>
2025-03-26 14:56:57 +0300

    Update string_are_arrays.py

--- module7/ex02/string_are_arrays.py
+++ module7/ex02/string_are_arrays.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python3
+import sys
 
+i = 0
+x = 0
+if len(sys.argv) != 2:
+	print("none")
+else:
+	while x < len(sys.argv[1]):
+		if sys.argv[1][x] == 'z':
+			i += 1
+		x += 1
+	if i != 0:
+		print("z" * i)
+	else:
+		print("none")
