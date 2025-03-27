bf138f6bb06a1dc2e7bdfbeaab313790acd6e896
AbeerAlmadadha <abeer.r.almadadha@gmail.com>
2025-03-27 03:08:48 +0300

    Update greetings_for_all.py

--- module8/ex03/greetings_for_all.py
+++ module8/ex03/greetings_for_all.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 
-def	greetings(name):
+def	greetings(name=''):
+	if isinstance(name, str):
+		if name:
+			print(f"Hello, {name}.")
+		else:
+			print("Hello, noble stranger")
+	else:
+		print("Error! It was not a name.")
 
+greetings('Alexandra')
+greetings('Wil')
+greetings()
+greetings(42)
